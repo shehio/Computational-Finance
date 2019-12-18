210c9e9093339f74d48b5e54791f9f4fb5afe0e4
shehio <shehabyasser@gmail.com>
2019-12-17 18:35:49 -0800

    Added variance to the estimator.

--- Monte Carlo/areas.R
+++ Monte Carlo/areas.R
@@ -4,31 +4,44 @@
 monte_carlo_area = function(f_x, n, a, b)
 {
   x = runif(n, a, b)
-  return(mean(f_x(x)))
+  estimates = f_x(x)
+  mc_estimator = mean(estimates)
+  mc_variance = var(estimates)
+  return(c(mc_estimator, mc_variance))
 }
 
 ns = c(10, 100, 1000, 10 * 1000, 100 * 1000, 1000 * 1000, 10 * 1000 * 1000)
 for (i in 1:length(ns))
 {
   n = ns[i]
-  
+  print(paste("n = ", n))
   Ux  = function(x) 1
-  E_uniform = monte_carlo_area(Ux, n, 0, 1)
-  print(paste("Area of f(x) = 1 from 0 to 1: ", E_uniform, ", for n = ", n))
+  struct = monte_carlo_area(Ux, n, 0, 1)
+  E_uniform = struct[1]
+  V_uniform = struct[2]
+  print(paste("Area of f(x) = 1 from 0 to 1: ", E_uniform, ", with variance: ", V_uniform))
 
   linear  = function(x) x
-  E_x = monte_carlo_area(linear, n, 0, 1)
-  print(paste("Area of f(x) = x from 0 to 1: ", E_x, ", for n = ", n))
+  struct = monte_carlo_area(linear, n, 0, 1)
+  E_x = struct[1]
+  V_x = struct[2]
+  print(paste("Area of f(x) = x from 0 to 1: ", E_x, ", with variance: ", V_x))
   
   quadratic  = function(x) x ** 2
-  E_x2 = monte_carlo_area(quadratic, n, 0, 1)
-  print(paste("Area of f(x) = x ^ 2 from 0 to 1: ", E_x2, ", for n = ", n))
+  struct = monte_carlo_area(quadratic, n, 0, 1)
+  E_x2 = struct[1]
+  V_x2 = struct[2]
+  print(paste("Area of f(x) = x from 0 to 1: ", E_x2, ", with variance: ", V_x2))
   
   sinusoidal  = function(x) sin(x)
-  E_sin = monte_carlo_area(sinusoidal, n, 0, 1)
-  print(paste("Area of f(x) = sin(x) from 0 to 1: ", E_sin, ", for n = ", n))
+  struct = monte_carlo_area(sinusoidal, n, 0, 1)
+  E_sin = struct[1]
+  V_sin = struct[2]
+  print(paste("Area of f(x) = x from 0 to 1: ", E_sin, ", with variance: ", V_sin))
   
   exponential  = function(x) exp(x)
-  E_exp = monte_carlo_area(exponential, n, 0, 1)
-  print(paste("Area of f(x) = e ^ x from 0 to 1: ", E_exp, ", for n = ", n))
+  struct = monte_carlo_area(exponential, n, 0, 1)
+  E_exp = struct[1]
+  V_exp = struct[2]
+  print(paste("Area of f(x) = x from 0 to 1: ", E_exp, ", with variance: ", V_exp))
 }
